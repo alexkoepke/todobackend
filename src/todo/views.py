449439223182d4107a922f093c5a084ae75d6bcc
Alex Koepke <akoepke@usdigitalpartners.com>
2018-06-28 08:50:05 -0400

    add tests applicaiton still works

--- src/todo/views.py
+++ src/todo/views.py
@@ -6,15 +6,18 @@ from rest_framework.reverse import reverse
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
+# Create your views here.
 class TodoItemViewSet(viewsets.ModelViewSet):
-    queryset = TodoItem.objects.all()
-    serializer_class = TodoItemSerializer
+  queryset = TodoItem.objects.all()
+  serializer_class = TodoItemSerializer
 
-    def perform_create(self, serializer):
-        instance = serializer.save()
-        instance.url = reverse('todoitem-detial', args=[instance.pk], request=self.request)
-        instance.save()
+  def perform_create(self, serializer):
+    # Save instance to get primary key and then update URL
+    instance = serializer.save()
+    instance.url = reverse('todoitem-detail', args=[instance.pk], request=self.request)
+    instance.save()
 
-    def delete(self, request):
-        TodoItem.objects.all().delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+  # Deletes all todo items
+  def delete(self, request):
+    TodoItem.objects.all().delete()
+    return Response(status=status.HTTP_204_NO_CONTENT) 
