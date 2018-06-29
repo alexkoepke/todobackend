49db2d275e8c73109121bbe2bd85385a30cfddec
Alex Koepke <akoepke@usdigitalpartners.com>
2018-06-29 15:43:28 -0400

    start Creating Releases using Docker

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,12 @@
 # Activate virtal env
 . /appenv/bin/activate
 
+
+# Download requirements to build cache
+pip download -d /build -r requirements_test.txt --no-input
+
 # Install app test reqs.
-pip install -r requirements_test.txt
+pip install --no-index -f /build -r requirements_test.txt
 
 # Run test.sh args
 exec $@
